bf4ed1baa77038f72b75aada1a845bcee75100e2
Guillaume GAGNAIRE <contact@scrola.net>
2024-05-29 14:08:37 +0200

    Set content-security policy

--- src/lib/classes/SwaggerPlugin.ts
+++ src/lib/classes/SwaggerPlugin.ts
@@ -409,6 +409,7 @@ export class SwaggerPlugin {
     reply.type('text/html').send(`<!DOCTYPE html>
     <html lang="en">
       <head>
+        <meta http-equiv="Content-Security-Policy" content="default-src 'self' unpkg.com">
         <meta charset="UTF-8">
         <title>Swagger UI</title>
         <link rel="stylesheet" type="text/css" href="/swagger/swagger-ui.css" />
